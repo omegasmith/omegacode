79d486f167271b66a91aff79696261b70dbf7129
Dusty Smith <smithomega34@gmail.com>
2021-11-01 18:17:02 -0600

    ss

---
+++ omega1.py
@@ -0,0 +1,56 @@
+#
+# age=20
+#price=19.50
+#first_name = "Mosh"
+#is_online = False
+#print ('hello world')
+#print (age)
+#print (price)
+#birth_year = input ("What year were you born?? ")
+# print ("Hello " + name)
+#birth_year = 2020 - int(birth_year)
+#print (birth_year)
+#first =  float(input ("First): "))
+#second = input ("Second: ")
+#sum = float(first) + float(second)
+#print (" Sum is: " + str(sum))
+#course = 'Python for Me'
+#print (course.find('Python'))
+#print ('Python' in course)
+#
+#x = 10
+#x += 3
+#print (x)
+#x = 3 != 2
+#print (x)
+#price = 5
+#print (price > 10 or not price >2 )
+#temp =  10
+#if temp == 30:
+#    print ("its a hot day")
+#elif temp > 40:
+#    print ("drink water")
+#else:
+#    print ("wow")
+#print ("Done")
+#i = 1
+#while i <= 10 :
+#    print(i * 'f')
+#    i += 1
+#
+#names = ["John", "Bob", "Mosh"]
+#print (names[0])
+#names[0] = "Jon"
+#print (names [0:3])
+#
+# highest value in list
+#list = [1, 7, 3, 4, 5]
+#max = list[0]
+#for number in list:
+#    if number > max:
+#        max = number
+#print (max)
+#
+#
+
+
